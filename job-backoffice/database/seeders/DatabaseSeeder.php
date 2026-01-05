98b8a6c73b81adbe637e532f6a59e654fc4e5b4b
mhammadgamal <mohamed.gamal@dafagate.com>
2026-01-05 17:29:03 +0200

    add job applications and job data JSON files; update DatabaseSeeder to populate job categories, companies, and job vacancies

--- job-backoffice/database/seeders/DatabaseSeeder.php
+++ job-backoffice/database/seeders/DatabaseSeeder.php
@@ -2,9 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Company;
+use App\Models\JobCategory;
+use App\Models\JobVacancy;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +22,62 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::firstOrCreate(['email' => 'admin@admin.com'], [
+            'name' => 'Admin',
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345678'),
+            'role' => 'admin',
         ]);
+
+        // Seed data to test with
+        $jsonData = json_decode(file_get_contents(database_path('data/job_data.json')), true);
+
+        // create job categories
+        foreach ($jsonData['jobCategories'] as $category) {
+            JobCategory::firstOrCreate([
+                'name' => $category
+            ]);
+        }
+
+        // companies
+        foreach ($jsonData['companies'] as $company) {
+
+            // create owner user for company
+            $ownerUser = User::firstOrCreate([
+                'email' => fake()->unique()->safeEmail(),
+            ], [
+                'name' => fake()->name(),
+                'password' => Hash::make('12345678'),
+                'role' => 'company-owner',
+                'email_verified_at' => now(),
+            ]);
+            Company::firstOrCreate([
+                'name' => $company['name'],
+            ], [
+                'website' => $company['website'],
+                'address' => $company['address'],
+                'industry' => $company['industry'],
+                'ownerId' => $ownerUser->id,
+            ]);
+        }
+
+
+        foreach ($jsonData['jobVacancies'] as $jobVacancy) {
+            // get company
+            $company = Company::where('name', $jobVacancy['company'])->firstOrFail();
+
+            // get category
+            $category = JobCategory::where('name', $jobVacancy['category'])->firstOrFail();
+
+            JobVacancy::firstOrCreate(['title' => $jobVacancy['title'],], [
+                'title' => $jobVacancy['title'],
+                'description' => $jobVacancy['description'],
+                'location' => $jobVacancy['location'],
+                'salary' => $jobVacancy['salary'],
+                'type' => $jobVacancy['type'],
+                'company_id' => $company->id,
+                'category_id' => $category->id,
+            ]);
+        }
     }
 }
