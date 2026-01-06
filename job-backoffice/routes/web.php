13342556c6c496e75f5abf8c2e1aa23cb2cf1675
mhammadgamal <mohamed.gamal@dafagate.com>
2026-01-06 14:39:29 +0200

    feat: Introduce dedicated management sections for job applications, categories, vacancies, companies, and users, and restructure the dashboard.

--- job-backoffice/routes/web.php
+++ job-backoffice/routes/web.php
@@ -1,11 +1,20 @@
 <?php
 
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\JobApplicationController;
+use App\Http\Controllers\JobCategoryController;
+use App\Http\Controllers\JobVacancyController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/companies', [CompanyController::class, 'index'])->middleware(['auth', 'verified'])->name('companies');
+Route::get('/job-categories', [JobCategoryController::class, 'index'])->middleware(['auth', 'verified'])->name('job-categories');
+Route::get('/job-vacancies', [JobVacancyController::class, 'index'])->middleware(['auth', 'verified'])->name('job-vacancies');  
+Route::get('/job-applications', [JobApplicationController::class, 'index'])->middleware(['auth', 'verified'])->name('job-applications');
+Route::get('/users', [UserController::class, 'index'])->middleware(['auth', 'verified'])->name('users');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -13,4 +22,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
