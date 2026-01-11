af03f20d815ad5ea74b81e077baa7df920263de1
mhammadgamal <mohamed.gamal@dafagate.com>
2026-01-11 17:35:52 +0200

    feat: Revamp dashboard layout with overview cards for active users, total jobs, and applications

--- job-backoffice/resources/views/dashboard/index.blade.php
+++ job-backoffice/resources/views/dashboard/index.blade.php
@@ -5,13 +5,42 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+    <div class="py-12 px-6 flex flex-col gap-6">
+        {{-- Overview cards --}}
+        <div class="grid grid-cols-3 gap-6">
+            {{-- Active Users --}}
+            <div class="bg-white overflow-hidden shadow-sm rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("Dashboard") }}
+                    <h3 class="text-lg font-medium text-gray-900">{{ __('Active Users') }}</h3>
+                    <p class="text-3xl font-bold text-indigo-600">100</p>
+                    <p class="text-sm text-gray-500"> Last 30 days </p>
+                </div>
+            </div>
+            {{-- Total Jobs --}}
+            <div class="bg-white overflow-hidden shadow-sm rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <h3 class="text-lg font-medium text-gray-900">{{ __('Total Jobs') }}</h3>
+                    <p class="text-3xl font-bold text-indigo-600">100</p>
+                    <p class="text-sm text-gray-500"> All Time </p>
+                </div>
+            </div>
+            {{-- Total Application --}}
+            <div class="bg-white overflow-hidden shadow-sm rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <h3 class="text-lg font-medium text-gray-900">{{ __('Total Application') }}</h3>
+                    <p class="text-3xl font-bold text-indigo-600">100</p>
+                    <p class="text-sm text-gray-500"> All Time </p>
                 </div>
             </div>
         </div>
+    </div>
+    {{-- most Applied Jobs --}}
+    <div class="grid grid-cols-3 gap-6">
+        <div class="bg-white overflow-hidden shadow-sm rounded-lg">
+            <h3 class="text-lg font-medium text-gray-900">{{ __('Most Applied Jobs') }}</h3>
+        </div>
+    </div>
+    {{-- Conversion Rates --}}
+
     </div>
 </x-app-layout>
