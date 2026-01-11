6d0a4e2b1411c8193648fc1c67826d9549c07a50
mhammadgammal <mhammadgamal17@gmail.com>
2026-01-11 22:40:13 +0200

    design UI of dashboard

--- job-backoffice/resources/views/dashboard/index.blade.php
+++ job-backoffice/resources/views/dashboard/index.blade.php
@@ -35,12 +35,65 @@
         </div>
     </div>
     {{-- most Applied Jobs --}}
-    <div class="grid grid-cols-3 gap-6">
-        <div class="bg-white overflow-hidden shadow-sm rounded-lg">
-            <h3 class="text-lg font-medium text-gray-900">{{ __('Most Applied Jobs') }}</h3>
+    <div class="ms-6 p-6 bg-white overflow-hidden shadow-sm rounded-lg">
+        <h3 class="text-lg font-medium text-gray-900">{{ __('Most Applied Jobs') }}</h3>
+        {{-- application table --}}
+        <div>
+            <table class="w-full divide-y divide-gray-200">
+                <thead>
+                    <tr class="text-left">
+                        <th>Job Title</th>
+                        <th>Company</th>
+                        <th>Total Applications</th>
+                    </tr>
+                </thead>
+
+                <tbody class="divide-y divide-gray-200">
+                    <tr>
+                        <td class="py-4 whitespace-nowrap">Software Engineer</td>
+                        <td class="py-4 whitespace-nowrap">Tech Corp</td>
+                        <td class="py-4 whitespace-nowrap">150</td>
+                    </tr>
+                    <tr>
+                        <td class="py-4 whitespace-nowrap">Software Engineer</td>
+                        <td class="py-4 whitespace-nowrap">Tech Corp</td>
+                        <td class="py-4 whitespace-nowrap">150</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
-    {{-- Conversion Rates --}}
 
+    {{-- Conversion Rates --}}
+    <div class="ms-6 p-6 bg-white overflow-hidden shadow-sm rounded-lg">
+        <h3 class="text-lg font-medium text-gray-900">{{ __('Most Applied Jobs') }}</h3>
+        {{-- application table --}}
+        <div>
+            <table class="w-full divide-y divide-gray-200">
+                <thead>
+                    <tr class="text-left">
+                        <th>Job Title</th>
+                        <th>Views</th>
+                        <th>Applications</th>
+                        <th>Conversion Rate</th>
+                    </tr>
+                </thead>
+
+                <tbody class="divide-y divide-gray-200">
+                    <tr>
+                        <td class="py-4 whitespace-nowrap">Software Engineer</td>
+                        <td class="py-4 whitespace-nowrap">5</td>
+                        <td class="py-4 whitespace-nowrap">150</td>
+                        <td class="py-4 whitespace-nowrap">50%</td>
+                    </tr>
+                    <tr>
+                        <td class="py-4 whitespace-nowrap">Software Engineer</td>
+                        <td class="py-4 whitespace-nowrap">5</td>
+                        <td class="py-4 whitespace-nowrap">150</td>
+                        <td class="py-4 whitespace-nowrap">50%</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </x-app-layout>
