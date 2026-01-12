02b67cfb07a3c06921f356b2bc2b0175b489af3f
mhammadgammal <mhammadgamal17@gmail.com>
2026-01-13 00:46:50 +0200

    Add routes for dashboard and job applications
    - Introduced a new route for the dashboard that points to the DashboardController.
    - Added a route for job applications that directs to the JobApplicationsController.

--- job-app/routes/web.php
+++ job-app/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+
+use App\Http\Controllers\JobApplicationsController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,6 +15,8 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/job-applications', [JobApplicationsController::class, 'index'])->name('job-applications.index');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
