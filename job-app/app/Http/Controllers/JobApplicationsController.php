b5c42c2611c596a7366716af601a203a88b3785f
mhammadgammal <mhammadgamal17@gmail.com>
2026-01-18 00:17:46 +0200

    Implement job applications index view and controller method

--- job-app/app/Http/Controllers/JobApplicationsController.php
+++ job-app/app/Http/Controllers/JobApplicationsController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class JobApplicationsController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('job-applications.index');
+    }
 }
